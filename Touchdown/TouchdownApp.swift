af8d82bb26b98c0496f0d469dcabbd60d7335396
Myron Dulay <dreiohc@live.com>
2021-06-08 11:56:23 +0800

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Myron Dulay on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
