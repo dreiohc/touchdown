d7659376525c0a166b8a6e889fad3687410faada
Myron Dulay <myron.dulay@accenture.com>
2021-08-12 10:58:53 +0800

    Create brand view and add it in Home view

--- Touchdown/View/Home/BrandGridView.swift
+++ Touchdown/View/Home/BrandGridView.swift
@@ -12,12 +12,24 @@ struct BrandGridView: View {
     
     // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, spacing: columnSpacing, content: {
+                ForEach(brands) { brand in
+                    BrandItemView(brand: brand)
+                }
+            }) //: GRID
+            .frame(height: 200)
+            .padding(15)
+        }) //: SCROLL
+        
+        
     }
 }
 
 struct BrandGridView_Previews: PreviewProvider {
     static var previews: some View {
         BrandGridView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
     }
 }
